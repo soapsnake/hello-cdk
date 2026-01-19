1c80317f9326f38f6ab49e17a216683cde965c62
Steven <d.liu@univapay.com>
2026-01-19 12:49:31 +0900

    init

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
+import { aws_lambda as lambda } from 'aws-cdk-lib';
+import path from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const HelloCdkLambda = new lambda.Function(this, 'HelloCdkHandler', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
+      handler: 'index.handler',
+    });
   }
 }
